90f53473783647a141e7ec295c9794bb2437996f
Chris Zuber <shgysk8zer0@gmail.com>
2019-01-01 15:42:30 -0800

    WIP login/registration forms

---
+++ components/login-form/login-form.js
@@ -0,0 +1,40 @@
+import {importLink} from '../../js/functions.js';
+
+export default class HTMLLoginFormElement extends HTMLElement {
+	constructor() {
+		super();
+		importLink('login-form-template').then(async form => {
+			const nodes = [...form.content.querySelectorAll('body > *')].map(node => node.cloneNode(true));
+			this.attachShadow({mode: 'open'}).append(...nodes);
+			this.form.addEventListener('submit', async event => {
+				event.preventDefault();
+				try {
+					const data = Object.fromEntries(new FormData(event.target).entries());
+					console.log(data);
+					this.form.reset();
+				} catch (error) {
+					console.error(error);
+				}
+			});
+			this.form.addEventListener('reset', () => this.dispatchEvent(new Event('reset')), {passive: true});
+		});
+	}
+
+	get method() {
+		return this.hasAttribute('method') ? this.getAttribute('method').toUpperCase() : 'POST' ;
+	}
+
+	set method(method) {
+		this.setAttribute('method', method);
+	}
+
+	get action() {
+		return new URL(this.getAttribute('method'), document.baseURI);
+	}
+
+	get form() {
+		return this.shadowRoot.querySelector('form');
+	}
+}
+
+customElements.define('login-form', HTMLLoginFormElement);
--- js/index.js
+++ js/index.js
@@ -2,5 +2,17 @@ import './std-js/deprefixer.js';
 import './std-js/shims.js';
 import '../components/current-year.js';
 import '../components/chat-app/chat-app.js';
+import '../components/login-form/login-form.js';
+import '../components/registration-form/registration-form.js';
 
 document.documentElement.classList.replace('no-js', 'js');
+document.documentElement.classList.toggle('no-dialog', document.createElement('dialog') instanceof HTMLUnknownElement);
+
+customElements.whenDefined('login-form').then(() => {
+	document.querySelector('login-form').addEventListener('reset', event => event.target.closest('dialog').close());
+});
+
+customElements.whenDefined('registration-form').then(() => {
+	document.getElementById('registration-form-dialog').showModal();
+	document.querySelector('registration-form').addEventListener('reset', event => event.target.closest('dialog').close());
+});
